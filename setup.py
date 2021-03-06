a43c451123497f9c398607aa0487670070ae5d09
bicarlsen <carlsen.bri@gmail.com>
2021-03-06 16:24:45 +0100

    Updated version.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ project_urls = {
 
 setuptools.setup(
     name = 'thot-data',
-    version = '0.4.10',
+    version = '0.5.0',
     author = 'Brian Carlsen',
     author_email = 'carlsen.bri@gmail.com',
     description = 'Python library for Thot data analysis and management.',
